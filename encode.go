f3e078c04863b1ccdbad3771f978646f0ae8ea83
K. Okabe <48155865+ksrnnb@users.noreply.github.com>
2022-11-03 11:49:06 +0900

    :recycling: divide files

---
+++ encode.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"unicode/utf8"
+
+	"github.com/ksrnnb/qrcode/bitset"
+)
+
+func EncodeRawData(ecl ErrorCorrectionLevel, src string) *bitset.BitSet {
+	if ecl != Medium {
+		panic("this app supports only 1-M type, error correction level must be 'M'")
+	}
+
+	if utf8.RuneCountInString(src) >= 16 {
+		panic("this app supports only 1-M type and 8 bits byte mode, must be less than 16 characters")
+	}
+
+	mode := EightBits
+	version := 1
+
+	// 1-M: data code size is 16
+	// 8 bytes mode => 16 * 8
+	codeLength := 16 * 8
+	bs := bitset.NewBitSet(codeLength)
+
+	bitCount := characterCountIndicatorBits(version, mode)
+	charCount := utf8.RuneCountInString(src)
+
+	addModeIndicator(bs, mode)
+	addCharacterCountIndicator(bs, bitCount, charCount)
+	addSrcData(bs, src)
+	addTerminator(bs)
+	addPaddingBit(bs)
+
+	return bs
+}
+
+// characterCountIndicatorBits returns character count indicater's bit numbers
+// reference: JIS X0510 : 2018 (ISO/IEC 18004 : 2015) Table 3
+func characterCountIndicatorBits(version int, mode ModeIndicator) int {
+	if 1 <= version && version <= 9 {
+		return version1To9CharacterCountIndicatorBits(mode)
+	} else if 10 <= version && version <= 26 {
+		return version10To26CharacterCountIndicatorBits(mode)
+	} else if 27 <= version && version <= 40 {
+		return version27To40CharacterCountIndicatorBits(mode)
+	}
+	return 0
+}
+
+func version1To9CharacterCountIndicatorBits(mode ModeIndicator) int {
+	switch mode {
+	case Numeric:
+		return 10
+	case AlphaNumeric:
+		return 9
+	case EightBits:
+		return 8
+	case Kanji:
+		return 8
+	default:
+		return 0
+	}
+}
+
+func version10To26CharacterCountIndicatorBits(mode ModeIndicator) int {
+	switch mode {
+	case Numeric:
+		return 12
+	case AlphaNumeric:
+		return 11
+	case EightBits:
+		return 16
+	case Kanji:
+		return 10
+	default:
+		return 0
+	}
+}
+
+func version27To40CharacterCountIndicatorBits(mode ModeIndicator) int {
+	switch mode {
+	case Numeric:
+		return 14
+	case AlphaNumeric:
+		return 13
+	case EightBits:
+		return 16
+	case Kanji:
+		return 12
+	default:
+		return 0
+	}
+}
+
+// addModeIndicator adds mode indicator and returns next position
+func addModeIndicator(bs *bitset.BitSet, mode ModeIndicator) {
+	bs.SetInt(int(mode), modeCharCount)
+}
+
+// addCharacterCountIndicator adds character count indicator and returns next position
+func addCharacterCountIndicator(bs *bitset.BitSet, bitCount int, charCount int) {
+	bs.SetInt(charCount, bitCount)
+}
+
+// addSrcData adds src data and returns next position
+func addSrcData(bs *bitset.BitSet, src string) {
+	// supports only 8 bit byte mode
+	for _, c := range src {
+		bs.SetByte(byte(c))
+	}
+}
+
+// addTerminator adds 0000 padding and returns next position
+func addTerminator(bs *bitset.BitSet) {
+	if bs.Position() == bs.Length() {
+		return
+	}
+
+	if bs.Position() <= bs.Length()-4 {
+		bs.SetInt(0, 4)
+		return
+	}
+
+	nextPos := bs.Position()
+	for i := nextPos; i < bs.Length(); i++ {
+		bs.SetBool(false)
+	}
+}
+
+// addZeroPadding add 0 padding if last bit string is not 8 bits
+func addZeroPadding(bs *bitset.BitSet) {
+	for i := 0; i < 8; i++ {
+		if bs.Position()%8 == 0 {
+			break
+		}
+		bs.SetBool(false)
+	}
+}
+
+// addPaddingBit add 0 padding if last bit string is not 8 bits
+func addPaddingBit(bs *bitset.BitSet) {
+	addZeroPadding(bs)
+	if bs.Position() == bs.Length() {
+		return
+	}
+
+	paddingPatterns := []int{0b11101100, 00010001}
+	for i := 0; bs.Position() < bs.Length(); i++ {
+		bs.SetInt(paddingPatterns[i%2], 8)
+	}
+}
---
+++ encode_test.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/ksrnnb/qrcode/bitset"
+)
+
+func TestCharacterCountIndicatorBits(t *testing.T) {
+	tests := []struct {
+		name    string
+		version int
+		mode    ModeIndicator
+		want    int
+	}{
+		{
+			name:    "version is less than minimum value",
+			version: 0,
+			mode:    Numeric,
+			want:    0,
+		},
+		{
+			name:    "version is greater than maximum value",
+			version: 41,
+			mode:    Numeric,
+			want:    0,
+		},
+		{
+			name:    "version is 1-9 and mode is numeric",
+			version: 1,
+			mode:    Numeric,
+			want:    10,
+		},
+		{
+			name:    "version is 1-9 and mode is alpha numeric",
+			version: 1,
+			mode:    AlphaNumeric,
+			want:    9,
+		},
+		{
+			name:    "version is 1-9 and mode is 8 bits byte",
+			version: 1,
+			mode:    EightBits,
+			want:    8,
+		},
+		{
+			name:    "version is 1-9 and mode is kanji",
+			version: 1,
+			mode:    Kanji,
+			want:    8,
+		},
+		{
+			name:    "version is 10-26 and mode is numeric",
+			version: 10,
+			mode:    Numeric,
+			want:    12,
+		},
+		{
+			name:    "version is 10-26 and mode is alpha numeric",
+			version: 10,
+			mode:    AlphaNumeric,
+			want:    11,
+		},
+		{
+			name:    "version is 10-26 and mode is 8 bits byte",
+			version: 10,
+			mode:    EightBits,
+			want:    16,
+		},
+		{
+			name:    "version is 10-26 and mode is kanji",
+			version: 10,
+			mode:    Kanji,
+			want:    10,
+		},
+		{
+			name:    "version is 27-40 and mode is numeric",
+			version: 27,
+			mode:    Numeric,
+			want:    14,
+		},
+		{
+			name:    "version is 27-40 and mode is alpha numeric",
+			version: 27,
+			mode:    AlphaNumeric,
+			want:    13,
+		},
+		{
+			name:    "version is 27-40 and mode is 8 bits byte",
+			version: 27,
+			mode:    EightBits,
+			want:    16,
+		},
+		{
+			name:    "version is 27-40 and mode is kanji",
+			version: 27,
+			mode:    Kanji,
+			want:    12,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := characterCountIndicatorBits(test.version, test.mode)
+			if result != test.want {
+				t.Errorf("expected %d, got %d\n", test.want, result)
+			}
+		})
+	}
+}
+
+func TestAddTerminator(t *testing.T) {
+	tests := []struct {
+		name     string
+		codeSize int
+		length   int
+		arg      int
+		wantPos  int
+	}{
+		{
+			name:     "equals to code length",
+			codeSize: 2,
+			length:   16,
+			arg:      0b1111_1111_1111_1111,
+			wantPos:  16,
+		},
+		{
+			name:     "less than code length and greater than code length-4",
+			codeSize: 2,
+			length:   13,
+			arg:      0b1_1111_1111_1111,
+			wantPos:  16,
+		},
+		{
+			name:     "less than code code length-4",
+			codeSize: 2,
+			length:   10,
+			arg:      0b11_1111_1111,
+			wantPos:  14,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			bs := bitset.NewBitSet(test.codeSize * 8)
+			bs.SetInt(test.arg, test.length)
+			addTerminator(bs)
+			if bs.Position() != test.wantPos {
+				t.Errorf("nextPos is expected %d, but got %d\n", test.wantPos, bs.Position())
+			}
+		})
+	}
+}
+
+func TestAddZeroPadding(t *testing.T) {
+	tests := []struct {
+		name   string
+		bsSize int
+		length int
+		arg    int
+		want   int
+	}{
+		{
+			name:   "last bit string is 8bits",
+			bsSize: 16,
+			length: 16,
+			arg:    0b1111_1111_1111_1111,
+			want:   0b1111_1111_1111_1111,
+		},
+		{
+			name:   "last bit string is not 8bits",
+			bsSize: 16,
+			length: 13,
+			arg:    0b1_1111_1111_1111,
+			want:   0b1111_1111_1111_1000,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			bs := bitset.NewBitSet(test.bsSize)
+			bs.SetInt(test.arg, test.length)
+			addZeroPadding(bs)
+
+			for i := 0; i < test.bsSize; i++ {
+				result := bs.GetValue(i)
+				want := (test.want>>(test.bsSize-1-i))&1 == 1
+				if result != want {
+					t.Errorf("expected %v, but got %v at pos %d\n", want, result, i)
+				}
+			}
+		})
+	}
+}

a1185462a4e948f6768c0f1f16709ab23617f455
K. Okabe <48155865+ksrnnb@users.noreply.github.com>
2022-11-05 15:43:31 +0900

    :+1: update main.go

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ksrnnb/qrcode/reedsolomon"
+)
 
 func main() {
 	// use only Medium to simplify
-	ecl := ECLIndicator("M")
+	data := EncodeRawData(ECL_Medium, "Hello, World!")
+	// 1-M: count of error collection words is 10
+	ecwords := 10
+	rsEncoded := reedsolomon.Encode(data, ecwords)
+
+	fmt.Printf("%+v\n", rsEncoded.Values())
+
+	// TODO: add regular symbol, evaluate mask,
 	modules := [][]bool{{}}
-	formatInfo := FormatInfo(ecl, modules)
+	formatInfo := FormatInfo(ECL_Medium, modules)
 	fmt.Println(formatInfo)
 
 	// EncodeAlphaNumericString("Hello")
